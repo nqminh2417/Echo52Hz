4065b2faa01c55eaaaaaf693d06150ecf9492ec6
nqminh2417 <quangminh2417@gmail.com>
2024-04-03 08:27:36 +0700

    update 20240403

--- lib/features/home/home_screen.dart
+++ lib/features/home/home_screen.dart
@@ -2,7 +2,6 @@ import 'package:firebase_auth/firebase_auth.dart';
 import 'package:flutter/material.dart';
 
 import '../authentication/login_screen.dart';
-import 'package:fluttertoast/fluttertoast.dart';
 
 class HomeScreen extends StatefulWidget {
   const HomeScreen({super.key});
@@ -12,8 +11,6 @@ class HomeScreen extends StatefulWidget {
 }
 
 class _HomeScreenState extends State<HomeScreen> {
-  bool _backButtonWasPressed = false;
-
   Future<void> _logout() async {
     await FirebaseAuth.instance.signOut();
     if (mounted) {
@@ -21,47 +18,33 @@ class _HomeScreenState extends State<HomeScreen> {
     }
   }
 
-  Future<bool> _onPopInvoked(bool result) async {
-    if (!_backButtonWasPressed) {
-      _backButtonWasPressed = true;
-      Fluttertoast.showToast(msg: "Press again to exit app");
-      Future.delayed(const Duration(seconds: 2), () => _backButtonWasPressed = false);
-      return false; // Prevent route popping immediately
-    }
-    return true; // Allow route popping on second press
-  }
-
   @override
   Widget build(BuildContext context) {
-    return PopScope(
-      canPop: false,
-      onPopInvoked: _onPopInvoked,
-      child: Scaffold(
-        appBar: AppBar(
-          // automaticallyImplyLeading: false,
-          centerTitle: true,
-          title: const Text(
-            'Home Screen',
-          ),
+    return Scaffold(
+      appBar: AppBar(
+        // automaticallyImplyLeading: false,
+        centerTitle: true,
+        title: const Text(
+          'Home Screen',
         ),
-        body: Center(
-          child: Column(
-            mainAxisAlignment: MainAxisAlignment.center,
-            children: [
-              const Text('Welcome to the Home Screen!'),
-              ElevatedButton(
-                onPressed: _logout,
-                child: const Text('Logout'),
-              ),
-            ],
-          ),
+      ),
+      body: Center(
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: [
+            const Text('Welcome to the Home Screen!'),
+            ElevatedButton(
+              onPressed: _logout,
+              child: const Text('Logout'),
+            ),
+          ],
         ),
-        floatingActionButton: FloatingActionButton(
-          onPressed: () {
-            // Add your action here, e.g., navigate to another screen
-          },
-          child: const Icon(Icons.add),
-        ),
+      ),
+      floatingActionButton: FloatingActionButton(
+        onPressed: () {
+          // Add your action here, e.g., navigate to another screen
+        },
+        child: const Icon(Icons.add),
       ),
     );
   }
