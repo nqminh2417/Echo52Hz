261077c1177f4ab015071e0baff59995ddbc3313
minh2417 <quangminh2417@gmail.com>
2024-09-11 16:27:02 +0700

    the movie db (2) display list trending movies

--- lib/features/the_movie_database/tmdb_screen.dart
+++ lib/features/the_movie_database/tmdb_screen.dart
@@ -12,6 +12,9 @@ class TmdbScreen extends StatefulWidget {
 
 class _TmdbScreenState extends State<TmdbScreen> {
   bool _apiKeySaved = false;
+  String _savedApiKey = '';
+  List<dynamic> _trendingMovies = [];
+  bool _isLoading = false;
 
   @override
   void initState() {
@@ -24,25 +27,52 @@ class _TmdbScreenState extends State<TmdbScreen> {
     final apiKey = await TmdbService.getApiKey();
     setState(() {
       _apiKeySaved = apiKey.isNotEmpty;
+      _savedApiKey = apiKey;
+      if (_apiKeySaved) {
+        _fetchTrendingMovies();
+      }
+    });
+  }
+
+  Future<void> _fetchTrendingMovies() async {
+    setState(() {
+      _isLoading = true;
+    });
+    final movies = await TmdbService.fetchTrendingMovies();
+    setState(() {
+      _trendingMovies = movies;
+      _isLoading = false;
     });
   }
 
   Future<void> _showSaveApiKeyDialog() async {
-    TextEditingController apiKeyController = TextEditingController();
+    TextEditingController apiKeyController = TextEditingController(text: _savedApiKey);
 
     return showDialog(
       context: context,
       barrierDismissible: false,
       builder: (context) => AlertDialog(
-        title: const Text('Enter API Key'),
-        content: TextField(
-          controller: apiKeyController,
-          decoration: const InputDecoration(hintText: 'Enter API Key'),
+        title: const Text('Manage API Key'),
+        content: Column(
+          mainAxisSize: MainAxisSize.min,
+          children: [
+            TextField(
+              controller: apiKeyController,
+              decoration: const InputDecoration(hintText: 'Enter API Key'),
+            ),
+            const SizedBox(height: 16),
+          ],
         ),
         actions: [
           TextButton(
             onPressed: () => Navigator.pop(context),
-            child: const Text('Cancel'),
+            child: const Text('Cancel', style: TextStyle(color: Colors.grey)),
+          ),
+          TextButton(
+            onPressed: () async {
+              apiKeyController.clear();
+            },
+            child: const Text('Clear', style: TextStyle(color: Colors.orange)),
           ),
           TextButton(
             onPressed: () async {
@@ -50,10 +80,12 @@ class _TmdbScreenState extends State<TmdbScreen> {
               await TmdbService.saveApiKey(apiKey);
               setState(() {
                 _apiKeySaved = true;
+                _savedApiKey = apiKey;
               });
               Navigator.pop(context);
+              _checkSavedApiKey();
             },
-            child: const Text('Save'),
+            child: const Text('Save', style: TextStyle(color: Colors.green)),
           ),
         ],
       ),
@@ -63,39 +95,46 @@ class _TmdbScreenState extends State<TmdbScreen> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(
-        // automaticallyImplyLeading: false,
-        centerTitle: true,
-        title: const Text(
-          'The Movie Database',
-        ),
-        actions: [
-          IconButton(
-            icon: Icon(
-              Icons.token,
-              color: _apiKeySaved ? Colors.greenAccent : Colors.redAccent,
-            ),
-            onPressed: () {
-              // _searchMovies();
-            },
+        appBar: AppBar(
+          // automaticallyImplyLeading: false,
+          centerTitle: true,
+          title: const Text(
+            'The Movie Database',
           ),
-        ],
-      ),
-      body: Center(
-        child: Column(
-          mainAxisAlignment: MainAxisAlignment.center,
-          children: [
-            if (_apiKeySaved)
-              // Display movie data or other content
-              const Text('API keys are saved. Ready to fetch movies!'),
-            if (!_apiKeySaved) const Text('Please save API keys to continue.'),
-            ElevatedButton(
-              onPressed: !_apiKeySaved ? _showSaveApiKeyDialog : null,
-              child: const Text('Save API Keys'),
+          actions: [
+            IconButton(
+              icon: Icon(
+                Icons.token,
+                color: _apiKeySaved ? Colors.greenAccent : Colors.redAccent,
+              ),
+              onPressed: () => _showSaveApiKeyDialog(),
             ),
           ],
         ),
-      ),
-    );
+        // if (!_apiKeySaved) const Text('Please save API keys to continue.'),
+        body: Center(
+          child: Column(
+            children: [
+              if (_isLoading) const Center(child: CircularProgressIndicator()),
+              if (_apiKeySaved && !_isLoading)
+                Expanded(
+                  child: ListView.builder(
+                    shrinkWrap: true,
+                    itemCount: _trendingMovies.length,
+                    itemBuilder: (context, index) {
+                      final movie = _trendingMovies[index];
+                      return ListTile(
+                        title: Text(movie['title']),
+                        onTap: () {
+                          // Navigate to a movie details screen
+                          // Navigator.push(context, MaterialPageRoute(builder: (context) => MovieDetailsScreen(movieId: movie['id'])));
+                        },
+                      );
+                    },
+                  ),
+                ),
+            ],
+          ),
+        ));
   }
 }
--- lib/features/the_movie_database/tmdb_service.dart
+++ lib/features/the_movie_database/tmdb_service.dart
@@ -1,8 +1,13 @@
+import 'dart:convert';
+
 import 'package:flutter_secure_storage/flutter_secure_storage.dart';
+import 'package:http/http.dart' as http;
 
 class TmdbService {
   static const _apiKeyKey = 'TMDB_API_KEY';
   static const _apiReadAccessTokenKey = 'TMDB_API_READ_ACCESS_TOKEN';
+
+  static const tmdbBaseUrl = 'https://api.themoviedb.org/3';
 
   static const _storage = FlutterSecureStorage();
 
@@ -22,5 +27,21 @@ class TmdbService {
 
   static Future<void> saveApiReadAccessToken(String apiReadAccessToken) async {
     await _storage.write(key: _apiReadAccessTokenKey, value: apiReadAccessToken);
+  }
+
+  static Future<List<dynamic>> fetchTrendingMovies() async {
+    final apiKey = await getApiKey();
+    final url = Uri.parse('$tmdbBaseUrl/trending/movie/day?api_key=$apiKey&language=vi-VI');
+
+    final response = await http.get(url);
+
+    if (response.statusCode == 200) {
+      final data = jsonDecode(response.body);
+      final List<dynamic> movies = data['results'];
+      return movies;
+    } else {
+      print('Error fetching trending movies: ${response.statusCode}');
+      return [];
+    }
   }
 }
